b1eefb739224a419a8ba4e9be6380956237b737e
WillStephensonxyz <WillStephenson66@gmail.com>
2024-09-05 16:44:30 +1000

    switched to subprocess for service monitoring

--- ServiceMonitoring/servicemonitoring.py
+++ ServiceMonitoring/servicemonitoring.py
@@ -1,13 +1,16 @@
 import os 
+import subprocess 
 
 services = ["sshd", "mysql", "apache2"] 
 
 def service_monitor(services):
-    
-    for service in services: 
-        if os.system(f"systemctl is-active --quiet {service}") == 0:
-            print(f"{service} is active") 
-        else:
-            print(f"{service} is inactive") 
+   
+    try: 
+        for service in services: 
+            if subprocess.run(["systemctl", "is-active", "--quiet", service], check=True) == 0:
+                print(f"{service} is active") 
+            else:
+                print(f"{service} is inactive, bring {service} back online") 
+                subprocess.run(["systemctl", "restart", service], check=True)
 
 service_monitor(services) 
